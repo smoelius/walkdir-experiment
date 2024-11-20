36e05d063449f00f4271e2262a58d2f86413c4d5
Samuel Moelius <samuel.moelius@trailofbits.com>
2024-11-19 19:06:45 -0500

    Flesh out

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::process::Command;
+use walkdir::WalkDir;
+
+fn main() {
+    let status = Command::new("git")
+        .args(["clone", "https://github.com/g-plane/pretty_yaml.git"])
+        .status()
+        .unwrap();
+    assert!(status.success());
+    let mut count = 0;
+    for result in WalkDir::new("pretty_yaml") {
+        let entry = result.unwrap();
+        let path = entry.path();
+        println!("{}", path.display());
+        count += 1;
+    }
+    println!("count = {count}");
+}
